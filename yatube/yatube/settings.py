43df27d5b0f5349dfe1e5a6c608da9a3b262b127
annrud <rudina_anna88@mail.ru>
2021-05-24 19:03:20 +0300

    Project of 7 sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,16 +1,26 @@
 
 import os
 
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')*+@k&)d_*fq95wo3**#tgwf)1-fe=xu8tpp+yxpshna50ndlc'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = ')*+@k&)d_*fq95wo3**#tgwf)1-fe=xu8tpp+yxpshna50ndlc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     'localhost',

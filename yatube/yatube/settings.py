566794d78c67f8f25a045f1c83cbda79ca9e30bb
annrud <rudina_anna88@mail.ru>
2021-05-26 16:20:27 +0300

    Project of 7 sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,10 +1,10 @@
 
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, 'yatube/.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 

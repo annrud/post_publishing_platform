970524cdbfbb7869ec553921f21ea9053b121d5a
annrud <rudina_anna88@mail.ru>
2021-10-17 20:01:21 +0800

    Fixed settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,7 +12,7 @@ load_dotenv(os.path.join(BASE_DIR, 'yatube/.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = [
      '*',
